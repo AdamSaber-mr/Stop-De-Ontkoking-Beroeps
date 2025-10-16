805a4aba0cfb7c63983276c57370979d528da390
André Smalheer <102464@glr.nl>
2025-10-16 13:28:24 +0000

    sseperated header js and css also added header 2

---
+++ js/header.js
@@ -0,0 +1,8 @@
+const header = document.getElementById('header');
+
+window.addEventListener('scroll', () => {
+if (window.scrollY > 50) {
+    header.classList.add('shrink');
+} else {
+    header.classList.remove('shrink');
+}});
