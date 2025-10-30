ae5ed93513eb6323b5060265d22e6ebfd1c7a64f
Adam Saber <102896@glr.nl>
2025-10-30 12:06:01 +0100

    De front end met de backend gelinkt

--- frontend_home/js/header.js
+++
@@ -1,8 +0,0 @@
-const header = document.getElementById('header');
-
-window.addEventListener('scroll', () => {
-if (window.scrollY > 50) {
-    header.classList.add('shrink');
-} else {
-    header.classList.remove('shrink');
-}});
