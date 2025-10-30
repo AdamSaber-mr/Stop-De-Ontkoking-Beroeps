79274c0e0840987d0cf8786f4425635da0cf802b
Adam Saber <102896@glr.nl>
2025-10-30 11:57:17 +0100

    backend gelinkt met de front end

---
+++ backend/header.php
@@ -0,0 +1,50 @@
+<?php
+ session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <title>Cook Up</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap" rel="stylesheet">
+
+    <link rel="icon" type="image/x-icon" href="images/logo.png">
+
+    <link rel="stylesheet" href="styles/index.css">
+    <link rel="stylesheet" href="styles/header.css">
+</head>
+<body>
+<div id="header">
+    <div id="logo-container">
+        <img id="logo" src="images/logo.png" alt="Logo">
+    </div>
+
+    <h1 id="site-title">Cook up</h1>
+
+    <ul id="nav-menu">
+        <li><a href="index.php">Home</a></li>
+        <li>Browse</li>
+        <li>Watch</li>
+    </ul>
+
+    <div id="account-section">
+        <?php
+        if (isset($_SESSION['useruid'])) {
+            echo "<li><a href='profile.php'>Profile page</a></li>";
+            echo "<li><a href='./includes/logout.inc.php'>Log out</a></li>";
+        }
+        else {
+            echo "<li><a href='signup.php'>Sign up</a></li>";
+            echo "<li><a href='login.php'>Login</a></li>";
+        }
+        ?>
+    </div>
+
+    <!-- <div id="acount">
+      <img src="images/user_placeholder.png">
+    </div> -->
+</div>
