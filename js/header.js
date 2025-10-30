951e6d952774c180e013105917916af70fe83603
André Smalheer <102464@glr.nl>
2025-10-30 08:48:35 +0000

    basic shorts page made

--- js/header.js
+++ js/header.js
@@ -6,3 +6,20 @@ if (window.scrollY > 50) {
 } else {
     header.classList.remove('shrink');
 }});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const account = document.getElementById("acount");
+  const dropdown = document.getElementById("header_ul");
+
+  account.addEventListener("click", (e) => {
+    e.stopPropagation(); 
+    dropdown.classList.toggle("show");
+  });
+
+  document.addEventListener("click", (e) => {
+    if (!account.contains(e.target) && !dropdown.contains(e.target)) {
+      dropdown.classList.remove("show");
+    }
+  });
+});
