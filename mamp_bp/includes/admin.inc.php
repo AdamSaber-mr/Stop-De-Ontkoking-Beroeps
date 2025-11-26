bf51f1b7867d782440be76dcd59e5e8679ecb52a
André Smalheer <102464@glr.nl>
2025-11-26 14:03:23 +0000

    admin now uses header.php

--- mamp_bp/includes/admin.inc.php
+++ mamp_bp/includes/admin.inc.php
@@ -15,38 +15,7 @@
     <link rel="stylesheet" href="../styles/admin.css">
 </head>
 <body>
-<div id="header">
-    <div id="logo-container">
-        <img id="logo" src="../images/logo.png" alt="Logo">
-    </div>
-
-    <h1 id="site-title">Cook up</h1>
-
-    <ul id="nav-menu">
-        <li><a href="../index.php">Home</a></li>
-        <li>Browse</li>
-        <li>Watch</li>
-    </ul>
-
-    <div id="account-section">
-        <?php
-        if (isset($_SESSION['useruid'])) {
-            if ($_SESSION['role'] === 'user') {
-                echo "<li><a href='../admin/admin.php'>Profile page</a></li>";
-                echo "<li><a href='../includes/logout.inc.php'>Log out</a></li>";
-            }
-            if ($_SESSION['role'] === 'admin') {
-                echo "<li><a href='../admin/admin.php'>Admin page</a></li>";
-                echo "<li><a href='../includes/logout.inc.php'>Log out</a></li>";
-            }
-        }
-        else {
-            echo "<li><a href='../signup.php'>Sign up</a></li>";
-            echo "<li><a href='../login.php'>Login</a></li>";
-        }
-        ?>
-    </div>
-</div>
+    <?php include './header.php'; ?>
 
     <div class="admin-shell">
         <aside class="admin-sidebar" aria-label="Admin navigatie">
